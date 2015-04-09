f5e1d0fbcd4631556141d9da54b0a395ed0d2e96
MichiLazarus <michaellazarus@hotmail.de>
2015-04-09 12:30:20 +0200

    Java Klassen an DB Verbindung angepasst Version 1.0

---
+++ app/src/main/java/connection/AppConfig.java
@@ -0,0 +1,12 @@
+package connection;
+
+/**
+ * Created by Michael on 09.04.2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://81.217.54.146/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://81.217.54.146/android_login_api/";
+}
